99e8bf1066096359f58577974d8ca0dfbb576bd3
Dadv-a11y <darilvia64@gmail.com>
2024-12-08 00:06:55 +0100

    updates

--- script.js
+++ script.js
@@ -1,72 +1,30 @@
-let workTimeframes=document.getElementById("workTimeframes");
-let playTimeframes= document.getElementById("playTimeframes");
-let studyTimeframes = document.getElementById("studyTimeframes");
-let ExerciseTimeframes = document.getElementById("ExerciseTimeframes");
-let socialTimeframes =document.getElementById("socialTimeframes");
-let selfcareTimeframes = document.getElementById("selfcareTimeframes");
-let current= 0 ;
-let h1 = document.createElement('h1');
-let p = document.createElement('p') ;
-
-fetch('/data.json').then((response) => {  
-    if(!response.ok) return console.log('Oops! Something went wrong.');
-    
-    return response.json();
-  }).then((data) => {
-   console.log(data)
-   if(current==1){
-    workTimeframes.appendChild(h1).innerHTML= data[0].timeframes.daily.current ;
-    workTimeframes.appendChild(p).innerHTML='last day'+' '+ data[0].timeframes.daily.previous +'-hrs' ;
-    playTimeframes.appendChild(h1).innerHTML= data[1].timeframes.daily.current;
-    playTimeframes.appendChild(p).innerHTML='last day'+' '+ data[1].timeframes.daily.previous +'-hrs';
-    studyTimeframes.appendChild(h1).innerHTML= data[2].timeframes.daily.current;
-    studyTimeframes.appendChild(p).innerHTML='last day'+' '+ data[2].timeframes.daily.previous +'-hrs';
-    ExerciseTimeframes.appendChild(h1).innerHTML= data[3].timeframes.daily.current;
-    ExerciseTimeframes.appendChild(p).innerHTML='last day'+' '+ data[3].timeframes.daily.previous +'-hrs';
-    socialTimeframes.appendChild(h1).innerHTML= data[4].timeframes.daily.current;
-    socialTimeframes.appendChild(p).innerHTML='last day'+' '+ data[4].timeframes.daily.previous +'-hrs';
-    selfcareTimeframes.appendChild(h1).innerHTML= data[5].timeframes.daily.current;
-     selfcareTimeframes.appendChild(p).innerHTML='last day'+' '+ data[5].timeframes.daily.previous +'-hrs';
-   }
-   else if ( current == 2){
-    workTimeframes.appendChild(h1).innerHTML= data[0].timeframes.weekly.current ;
-    workTimeframes.appendChild(p).innerHTML='last day'+' '+ data[0].timeframes.weekly.previous +'-hrs' ;
-    playTimeframes.appendChild(h1).innerHTML= data[1].timeframes.weekly.current;
-    playTimeframes.appendChild(p).innerHTML='last day'+' '+ data[1].timeframes.weekly.previous +'-hrs';
-    studyTimeframes.appendChild(h1).innerHTML= data[2].timeframes.weekly.current;
-    studyTimeframes.appendChild(p).innerHTML='last day'+' '+ data[2].timeframes.weekly.previous +'-hrs';
-    ExerciseTimeframes.appendChild(h1).innerHTML= data[3].timeframes.weekly.current;
-    ExerciseTimeframes.appendChild(p).innerHTML='last day'+' '+ data[3].timeframes.weekly.previous +'-hrs';
-    socialTimeframes.appendChild(h1).innerHTML= data[4].timeframes.weekly.current;
-    socialTimeframes.appendChild(p).innerHTML='last day'+' '+ data[4].timeframes.weekly.previous +'-hrs';
-    selfcareTimeframes.appendChild(h1).innerHTML= data[5].timeframes.weekly.current;
-    selfcareTimeframes.appendChild(p).innerHTML='last day'+' '+ data[5].timeframes.weekly.previous +'-hrs';
-   }
-   else if( current == 3){
-    workTimeframes.appendChild(h1).innerHTML= data[0].timeframes.monthly.current;
-   workTimeframes.appendChild(p).innerHTML='last day'+' '+ data[0].timeframes.monthly.previous +'-hrs' ;
-   playTimeframes.appendChild(h1).innerHTML= data[1].timeframes.monthly.current;
-   playTimeframes.appendChild(p).innerHTML='last day'+' '+ data[1].timeframes.monthly.previous +'-hrs';
-   studyTimeframes.appendChild(h1).innerHTML= data[2].timeframes.monthly.current;
-   studyTimeframes.appendChild(p).innerHTML='last day'+' '+ data[2].timeframes.monthly.previous +'-hrs';
-   ExerciseTimeframes.appendChild(h1).innerHTML= data[3].timeframes.monthly.current;
-   ExerciseTimeframes.appendChild(p).innerHTML='last day'+' '+ data[3].timeframes.monthly.previous +'-hrs';
-   socialTimeframes.appendChild(h1).innerHTML= data[4].timeframes.monthly.current;
-   socialTimeframes.appendChild(p).innerHTML='last day'+' '+ data[4].timeframes.monthly.previous +'-hrs';
-   selfcareTimeframes.appendChild(h1).innerHTML= data[5].timeframes.monthly.current;
-   selfcareTimeframes.appendChild(p).innerHTML='last day'+' '+ data[5].timeframes.monthly.previous +'-hrs';
-   }
-  
-  });
-  function dailybtn(){ 
-   current = 1 ;
-  }
-  function  weeklybtn(){
-   current=2 ;
-  }
-  function monthlybtn(){
-    current=3 ;
-  }
-  console.log(current)
-  
-  
+let daily = document.getElementById('daily');
+let monthly = document.getElementById('monthly');
+let weekly = document.getElementById('weekly');
+let dailybtns = document.getElementById('dailybtn');
+let monthlybtns = document.getElementById('monthlybtn');
+let weeklybtns = document.getElementById('weeklybtn');
+function dailybtn(){
+    daily.style.display ='block';
+    monthly.style.display =' none';
+    weekly.style.display =' none';
+    dailybtns.style.color ='white'
+    weeklybtns.style.color =' var(--DesaturatedBlue)'
+    monthlybtns.style.color =' var(--DesaturatedBlue)'
+}
+function weeklybtn(){
+    daily.style.display ='none';
+    monthly.style.display =' none';
+    weekly.style.display =' block';
+    weeklybtns.style.color ='white'
+    dailybtns.style.color =' var(--DesaturatedBlue)'
+    monthlybtns.style.color =' var(--DesaturatedBlue)'
+}
+function monthlybtn(){
+    daily.style.display ='none';
+    monthly.style.display =' block';
+    weekly.style.display =' none';
+    monthlybtns.style.color ='white'
+    weeklybtns.style.color =' var(--DesaturatedBlue)'
+    dailybtns.style.color =' var(--DesaturatedBlue)'
+}
